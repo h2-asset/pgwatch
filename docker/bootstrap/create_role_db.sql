f2031412aa1a27ac40d1a2c8793c0a2b1ffee7f3
Xan <xan@h2-asset.com.br>
2025-07-25 20:18:19 -0300

    🔧 Faz templating do arquivo de criação de roles e DBs

--- docker/bootstrap/create_role_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE ROLE pgwatch WITH 
-    IN ROLE pg_monitor
-    LOGIN PASSWORD 'pgwatchadmin';  -- change the pw for production
-
-CREATE DATABASE pgwatch OWNER pgwatch;
-
-CREATE DATABASE pgwatch_metrics OWNER pgwatch;
